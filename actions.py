797e67624761a3b1099959ec7e55038b8f6c578d
jessshen1032 <31828105+jessshen1032@users.noreply.github.com>
2017-10-29 14:55:14 -0400

    Update actions.py

--- actions.py
+++ actions.py
@@ -33,7 +33,6 @@ def get_time():
     now = datetime.datetime.now()
     hour = now.hour
     minute = now.minute
-    second= now.second
 
-    text = "The time is %d:%d" % (hour, minute, second)
+    text = "The time is %d:%d" % (hour, minute,)
     return text
