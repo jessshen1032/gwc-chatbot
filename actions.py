e66f38b4e61d0992a1b64cd4fe371d01cd1a4e2b
jessshen1032 <31828105+jessshen1032@users.noreply.github.com>
2017-11-12 15:10:40 -0500

    Update actions.py

--- actions.py
+++ actions.py
@@ -69,7 +69,7 @@ def get_randomfact():
             'While children of identical twins are legally first cousins, genetically they are half-siblings.',
             'The man who created pop-up ads has apologized to the world for creating one of the internet\'s most hated forms of advertising.', 
             'The reason why the wedding ring is placed on the fourth finger of your left hand is because it\'s the only finger that has a vein connected directly to your heart.', 
-            'The quietest room in the world is -9 decibels, quiet enough to hear your blood flowing.', 
+            'The quietest room in the world is -9 decibels, quiet enough for you to hear your blood flowing.', 
             'Zerao is a stadium in Brazil in which the midfield line supposedly lies exactly on the equator, making each team defend one hemisphere.', 
             'You once held a world record when you were born for being the \'Youngest Person on The Planet\'.']
     integer=random.randint(0,10)
