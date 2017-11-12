602ba06bf7eb80486de96249e90a0163d9c20e92
jessshen1032 <31828105+jessshen1032@users.noreply.github.com>
2017-11-12 15:07:09 -0500

    Update actions.py

--- actions.py
+++ actions.py
@@ -67,11 +67,11 @@ def get_randomfact():
             'There is a 51% chance that a flipped coin will land on the side that was facing up when it was flipped.', 
             'Sunglasses make humans look more attractive because they give the illusion of a chiseled bone structure on top of a soft-featured face.', 
             'While children of identical twins are legally first cousins, genetically they are half-siblings.',
-            'The man who created pop-up ads has apologized to the world for creating one of the internet's most hated forms of advertising.', 
-            'The reason why the wedding ring is placed on the fourth finger of your left hand is because it's the only finger that has a vein connected directly to your heart.', 
+            'The man who created pop-up ads has apologized to the world for creating one of the internet\'s most hated forms of advertising.', 
+            'The reason why the wedding ring is placed on the fourth finger of your left hand is because it\'s the only finger that has a vein connected directly to your heart.', 
             'The quietest room in the world is -9 decibels, quiet enough to hear your blood flowing.', 
             'Zerao is a stadium in Brazil in which the midfield line supposedly lies exactly on the equator, making each team defend one hemisphere.', 
-            'You once held a world record when you were born for being the 'Youngest Person on The Planet'.']
+            'You once held a world record when you were born for being the \'Youngest Person on The Planet\'.']
     integer=random.randint(0,10)
     
     text= "The fact is %s" % (facts[integer])
